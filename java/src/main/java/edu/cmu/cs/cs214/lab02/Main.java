d09a03b9ea245bc7f65533e1e0c3f891e7fbe85b
Huslenc <otgoohuslen0918@gmail.com>
2026-02-11 13:55:18 +0800

    Talbai oloh

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
+
         Rectangle rectangle = new Rectangle(2, 3);
+        Circle circle = new Circle(5);
+        Square square = new Square(4);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer r1 = new Renderer(rectangle);
+        Renderer r2 = new Renderer(circle);
+        Renderer r3 = new Renderer(square);
 
-        renderer.draw();
+        r1.draw();
+        r2.draw();
+        r3.draw();
     }
 }
